13a025a4429d6e71b32a6938e50f9ace75d73959
obasanz087 <access_3156@yahoo.co.jp>
2024-10-21 18:41:59 +0900

    clock update

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,19 @@
 //年・月・日・曜日を書き出す
-let date_update = document.getElementById('today')
+function header_date_update() {
     var today=new Date(); 
     var year = today.getFullYear();
     var month = today.getMonth()+1;
     var week = today.getDay();
     var day = today.getDate();
     var week_en= new Array("Sun.","Mon.","Tue.","Wed.","Thu.","Fri.","Sat.");
-    date_update.innerHTML = `${year+"."+month+"."+day+" "+week_en[week]}` //div要素の中身の変更
+    var hour = today.getHours();
+    var minute = ('00' + today.getMinutes()).slice(-2);
+    var second = ('00' + today.getSeconds()).slice(-2);
+    let date_update = document.getElementById('today')
+    date_update.innerHTML = `${year+"."+month+"."+day+" "+week_en[week]+'　'+hour+":"+minute+":"+second}`; //div要素の中身の変更
+}
+
+setInterval('header_date_update()',1000);
 
 //header_menu_btn
 
