61a1ebf022584b9f91a712459ba739d96152a155
obasanz087 <access_3156@yahoo.co.jp>
2024-11-17 23:47:55 +0900

    test

--- assets/script.js
+++ assets/script.js
@@ -38,3 +38,22 @@ $(function() {
         $('.header_nav').removeClass('active');
     });
   });
+
+//ページ遷移
+$(window).on('load', function(){
+    $('body').removeClass('fade');
+  });
+  $(function() {
+    // ハッシュリンク(#)と別ウィンドウでページを開く場合はスルー
+    $('a:not([href^="#"]):not([target])').on('click', function(e){
+      e.preventDefault(); // ナビゲートをキャンセル
+      url = $(this).attr('href'); // 遷移先のURLを取得
+      if (url !== '') {
+        $('body').addClass('fade'); // bodyに class="fadeout"を挿入
+        setTimeout(function(){
+          window.location = url; // 0.8秒後に取得したURLに遷移
+        }, 800);
+      }
+      return false;
+    });
+  });
